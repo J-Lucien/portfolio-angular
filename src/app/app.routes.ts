684fdcd9b66e80f681cabd307d053fbe0beeed86
J-Lucien <jtolojanahary5@gmail.com>
2024-08-18 20:34:53 +0300

    Migration: Transition project structure to Angular with integrated HTML and SCSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component: HomeComponent},
+    {path:'about-me',component:AboutMeComponent},
+    {path:'contact',component:ContactComponent}
+];
