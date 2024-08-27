f6ce5c0005748111ffb6b17a29860e4159512936
J-Lucien <jtolojanahary5@gmail.com>
2024-08-27 22:03:27 +0300

    Add SpeedDial menu for small screens to improve navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { MenuItem, MessageService } from 'primeng/api';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { ToastModule } from 'primeng/toast';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink,RouterLinkActive,SpeedDialModule,ToastModule],
+  providers: [MessageService],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  items!: MenuItem[] ;
 
+  constructor(private messageService: MessageService) {}
+
+    ngOnInit() {
+        this.items = [
+            {
+                icon: 'pi pi-home',
+                routerLink:['home']
+            },
+            {
+                icon: 'pi pi-user',
+                routerLink:['about-me']
+            },
+            {
+                icon: 'pi pi-envelope',
+                routerLink:['contact']
+            }
+        ];
+    }
 }
